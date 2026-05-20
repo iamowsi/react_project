5071abe47b24e36ae095b34a520878b501f34e63
Mustafa Baqeri <mowsibg@gmail.com>
2026-05-20 22:54:35 +0330

    probs-session

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import FunctionalComponent from './FunctionalComponent';
-import ClassComponent from './ClassComponent';
+import Car from './Car';   // این خط را اضافه کن
 
-const App = () => {
+function App() {
   return (
-    <>
-      <FunctionalComponent />
-      <ClassComponent />
-    </>
+    <div>
+      <Car make="toyota" model="camry" year="2012" color="blue" money="$5000"/>
+      <Car make="honda" model="125cdi" year="2025" color="red" money="$500"/>
+    </div>
   );
-};
+}
 
 export default App;
--- src/ClassComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-
-class ClassComponent extends Component {
-  render() {
-    return <div>سلام از کلاس کامپوننت</div>;
-  }
-}
-export default ClassComponent;
