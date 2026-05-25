d51375823ccd0d6c0a742b2ede486445329622c9
Mustafa Baqeri <mowsibg@gmail.com>
2026-05-25 11:17:39 +0330

    learn-map-key

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from "react";
+import UserInfo from "./UserInfo";
+
 function App() {
-  const [text,setText] = useState("hello world junirour developer");
-  const changText=()=>{
-    setText("Hello World Hero");
-  };
+  const users =[
+    {
+      name : "mustafabaqeri",age:"22",email:"mowsibg@gmail.com"
+    },
+    {
+      name : "ashkandejageh",age:"32",email:"dejag23456@gmail.com"      
+    },
+    {
+    name : "baqeri",age:"12",email:"wsibg@gmail.com"
+    }
+  ]
+
   return (
     <div>
-      <div id="text">{text}</div>
-      <button  onClick={changText}>Click Me</button>
-    </div>
-  );
-}
+      {users.map((user)=>(
+        <UserInfo key={user.name} name={user.name} age={user.age} email={user.email}/>
+      )
 
+      )}
+    </div>
+  )
+
+};
 export default App;
